1663ea1487bec679c364857a4f8bf624e00678c6
AbdulBasit-Khan <kbasit672@gmail.com>
2024-10-11 19:48:41 +0500

    c

--- lib/ui/login/login_screen.dart
+++ lib/ui/login/login_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter_blo/bloc/login/bloc/login_bloc.dart';
+import 'package:flutter_blo/bloc/login/bloc/login_event.dart';
+import 'package:flutter_blo/bloc/login/bloc/login_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -9,46 +12,74 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  late LoginBloc _loginBloc;
   final emailFocusNode = FocusNode();
   final passwordFocusNode = FocusNode();
+  @override
+  void initState() {
+    super.initState();
+    _loginBloc = LoginBloc();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Login'),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextFormField(
-              keyboardType: TextInputType.emailAddress,
-              focusNode: emailFocusNode,
-              decoration: const InputDecoration(
-                hintText: "Email",
-                border: OutlineInputBorder(),
+      body: BlocProvider(
+        create: (context) => _loginBloc,
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              BlocBuilder<LoginBloc, LoginState>(
+                buildWhen: (current, previous) =>
+                    current.email != previous.email,
+                builder: (context, state) {
+                  return TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    focusNode: emailFocusNode,
+                    decoration: const InputDecoration(
+                      hintText: "Email",
+                      border: OutlineInputBorder(),
+                    ),
+                    onChanged: (val) {
+                      context.read<LoginBloc>().add(EmailChanged(email: val));
+                    },
+                    onFieldSubmitted: (val) {},
+                  );
+                },
               ),
-              onChanged: (val) {},
-              onFieldSubmitted: (val) {},
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              keyboardType: TextInputType.text,
-              focusNode: passwordFocusNode,
-              decoration: const InputDecoration(
-                  hintText: "Password", border: OutlineInputBorder()),
-              onChanged: (val) {},
-              onFieldSubmitted: (val) {},
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            ElevatedButton(onPressed: () {}, child: const Text("Login"))
-          ],
+              const SizedBox(
+                height: 20,
+              ),
+              BlocBuilder<LoginBloc, LoginState>(
+                buildWhen: (current, previous) =>
+                    current.password != previous.password,
+                builder: (context, state) {
+                  return TextFormField(
+                    keyboardType: TextInputType.text,
+                    focusNode: passwordFocusNode,
+                    decoration: const InputDecoration(
+                        hintText: "Password", border: OutlineInputBorder()),
+                    onChanged: (val) {
+                      context
+                          .read<LoginBloc>()
+                          .add(PasswordChanged(password: val));
+                    },
+                    onFieldSubmitted: (val) {},
+                  );
+                },
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              ElevatedButton(onPressed: () {}, child: const Text("Login"))
+            ],
+          ),
         ),
       ),
     );
